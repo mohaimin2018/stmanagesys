fdc6b32c21ebb289390b247e285e9e6be9639290
Mohai <mohai.cse@gmail.com>
2022-10-20 12:28:19 +0600

    Project is done

--- readme.txt
+++ readme.txt
@@ -2,8 +2,8 @@ Admin
 Email: admin@gmail.com
 Password: admin
 Staff/Teacher
-Email: kanta@gmail.com
-Password: kanta
+Email: akibiqbal@gmail.com
+Password: akibiqbal
 Student
 Email: nahid@gmail.com
 Password: nahid
--- student_management_app/StudentViews.py
+++ student_management_app/StudentViews.py
@@ -195,6 +195,53 @@ def student_view_result(request):
     return render(request, "student_template/student_view_result.html", context)
 
 
+def payment(request):
+    student = Students.objects.get(admin=request.user.id) # Getting Logged in Student Data
+    course = student.course_id # Getting Course Enrolled of LoggedIn Student
+    # course = Courses.objects.get(id=student.course_id.id) # Getting Course Enrolled of LoggedIn Student
+    subjects = Subjects.objects.filter(course_id=course) # Getting the Subjects of Course Enrolled
+    context = {
+        "subjects": subjects
+    }
+    return render(request, "student_template/payment.html", context)
 
+
+def payment_post(request):
+    if request.method != "POST":
+        messages.error(request, "Invalid Method")
+        return redirect('[payment]')
+    else:
+        # Getting all the Input Data
+        subject_id = request.POST.get('subject')
+        start_date = request.POST.get('start_date')
+        end_date = request.POST.get('end_date')
+
+        # Parsing the date data into Python object
+        start_date_parse = datetime.datetime.strptime(start_date, '%Y-%m-%d').date()
+        end_date_parse = datetime.datetime.strptime(end_date, '%Y-%m-%d').date()
+
+        # Getting all the Subject Data based on Selected Subject
+        subject_obj = Subjects.objects.get(id=subject_id)
+        # Getting Logged In User Data
+        user_obj = CustomUser.objects.get(id=request.user.id)
+        # Getting Student Data Based on Logged in Data
+        stud_obj = Students.objects.get(admin=user_obj)
+
+        # Now Accessing Attendance Data based on the Range of Date Selected and Subject Selected
+        attendance = Attendance.objects.filter(attendance_date__range=(start_date_parse, end_date_parse), subject_id=subject_obj)
+        # Getting Attendance Report based on the attendance details obtained above
+        attendance_reports = AttendanceReport.objects.filter(attendance_id__in=attendance, student_id=stud_obj)
+
+        # for attendance_report in attendance_reports:
+        #     print("Date: "+ str(attendance_report.attendance_id.attendance_date), "Status: "+ str(attendance_report.status))
+
+        # messages.success(request, "Attendacne View Success")
+
+        context = {
+            "subject_obj": subject_obj,
+            "attendance_reports": attendance_reports
+        }
+
+        return render(request, 'student_template/payment_data.html', context)
 
 
